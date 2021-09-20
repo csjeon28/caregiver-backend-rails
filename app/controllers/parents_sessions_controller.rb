f54966de89d2eb03ab6e8549124437d96c5e0759
Christine Jeon <csjeon28@gmail.com>
2021-09-20 15:56:46 -0400

    took out auto-login option for now

---
+++ app/controllers/parents_sessions_controller.rb
@@ -0,0 +1,22 @@
+class ParentsSessionsController < ApplicationController
+    def create
+        @parent = Parent.find_by(email: session_params[:email])
+        if @parent.authenticate(session_params[:password])
+            token = JWT.encode({ parent_id: @parent.id }, ENV['SECRET_KEY'])
+            # render json: { parent: ParentSerializer.new(parent) token: token }
+            render json: { token: token }
+        else
+            render json: { errors: "Email/Password is incorrect." }, status: :unauthorized
+        end
+    end
+
+    # def auto_login
+    #     render json: {parent: ParentSerializer.new(current_parent)}
+    # end
+      
+    private
+    
+    def session_params
+        params.require(:parent).permit(:email, :password)
+    end
+end
