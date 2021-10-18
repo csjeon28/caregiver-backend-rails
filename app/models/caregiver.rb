f9328630efa7890291c0b260f07f1b10e0b1fdb3
Christine Jeon <csjeon28@gmail.com>
2021-10-17 22:09:53 -0400

    added favorites to parents & caregivers

--- app/models/caregiver.rb
+++ app/models/caregiver.rb
@@ -2,6 +2,7 @@ class Caregiver < ApplicationRecord
     has_secure_password
     has_many :jobs
     has_many :parents, through: :jobs
+    has_many :candidates
 
     validates :email, uniqueness: true
 end
