5c2fec2a9fc7b0c8579917c0745183739280b736
Christine Jeon <csjeon28@gmail.com>
2021-10-17 22:10:50 -0400

    added favorites to both parents & caregivers

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -3,6 +3,9 @@ class Parent < ApplicationRecord
     has_many :schedules
     has_many :jobs, through: :schedules
     has_many :caregivers, through: :jobs
+    has_many :candidates
+    has_many :caregiver_favorites, through: :caregivers
+    has_many :parent_favorites
 
     validates :email, uniqueness: true
 end
