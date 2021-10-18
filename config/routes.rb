5788088a56855578374632e447780cfdcc86265f
Christine Jeon <csjeon28@gmail.com>
2021-10-17 22:12:30 -0400

    added favorites routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,21 @@
 Rails.application.routes.draw do
-      # resources :parents, only: [:index, :show, :create, :update]
-      # resources :caregivers, only: [:index, :show, :create, :update]
-      # resources :jobs 
+  
+  resources :parent_favorites
+  resources :caregiver_favorites
+  
+  resources :caregivers do
+    resources :jobs
+    resources :candidates
+  end
+  post "caregiver/login", to:"caregivers#login"
+  get "caregiver/auto_login", to:"caregivers#auto_login"
 
-      # post '/parents/login', to: 'parents_auth#create'
-      # post '/caregivers/login', to: 'caregivers_auth#create'
-      # get '/home', to: 'home#show', only: [:show]
-
-      resources :caregivers do
-        resources :jobs
-      end
-      post "caregiver/login", to:"caregivers#login"
-      get "caregiver/auto_login", to:"caregivers#auto_login"
-
-      resources :parents do
-        resources :schedules
-        resources :jobs
-      end
-      post "parent/login", to:"parents#login"
-      get "parent/auto_login", to:"parents#auto_login"
+  resources :parents do
+    resources :schedules
+    resources :jobs
+    resources :candidates
+  end
+  post "parent/login", to:"parents#login"
+  get "parent/auto_login", to:"parents#auto_login"
 
 end
