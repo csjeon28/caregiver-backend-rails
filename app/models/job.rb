a573dfe1a9229a13d11d8432f44e920cebbbb3ec
Christine Jeon <csjeon28@gmail.com>
2021-10-17 22:29:40 -0400

    jobs is not functioning correctly yet

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,17 @@ class Job < ApplicationRecord
     belongs_to :parent
     belongs_to :caregiver
     belongs_to :schedule
+    has_many :candidates
+
+    def self.caregiver_jobs(id)
+        Schedule.left_outer_joins(:jobs).where(jobs: { caregiver_id: id })
+    end
+    
+    def self.parent_jobs(id)
+        Schedule.left_outer_joins(:jobs).where(jobs: { parent_id: id })
+    end
+
+    def self.available_jobs
+        Job.all.select{ |job| !job.caregiver_id }
+    end
 end
