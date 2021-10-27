4f8bc5399546b4a9191d869bc9e64b73be88ff7e
Christine Jeon <csjeon28@gmail.com>
2021-10-27 16:39:15 -0400

    added validations for registration pages for both caregivers & parents

--- app/models/caregiver.rb
+++ app/models/caregiver.rb
@@ -4,5 +4,16 @@ class Caregiver < ApplicationRecord
     has_many :parents, through: :jobs
     has_many :candidates
 
+    PASSWORD_REQ = /\A
+    (?=.{8,}) # Minimum 8 characters
+    (?=.*\d) # Contains at least one number
+    (?=.*[a-z]) # Contains at least one lowercase letter
+    (?=.*[A-Z]) # Contains at least one uppercase letter
+    (?=.*[[:^alnum:]]) # Contains at least one symbol
+    /x
+    
+    validates :password, :email, :first_name, :last_name, :date_of_birth, :hourly_rate, :city, :state, :language, presence: true
+    validates :password, format: PASSWORD_REQ
     validates :email, uniqueness: true
+    validates :hourly_rate, numericality: true
 end
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,8 +1,19 @@
 class Parent < ApplicationRecord
     has_secure_password
-    has_many :jobs, through: :schedules
+    has_many :jobs
     has_many :caregivers, through: :jobs
     has_many :candidates
 
+    PASSWORD_REQ = /\A
+    (?=.{8,}) # Minimum 8 characters
+    (?=.*\d) # Contains at least one number
+    (?=.*[a-z]) # Contains at least one lowercase letter
+    (?=.*[A-Z]) # Contains at least one uppercase letter
+    (?=.*[[:^alnum:]]) # Contains at least one symbol
+    /x
+    
+    validates :password, :email, :first_name, :last_name, :hourly_rate, :city, :state, :language, :number_of_children, presence: true
+    validates :password, format: PASSWORD_REQ
     validates :email, uniqueness: true
+    validates :hourly_rate, numericality: true
 end
