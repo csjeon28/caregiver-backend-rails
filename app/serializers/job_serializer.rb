223c86a57052774646aaf51b2106e70a1de55465
Christine Jeon <csjeon28@gmail.com>
2021-10-28 16:46:22 -0400

    added parent_id to job serializer

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,4 +1,4 @@
 class JobSerializer < ActiveModel::Serializer
-    attributes :id, :title, :hourly_rate, :job_description, :number_of_children, :specific_days_needed, :required_to_drive
+    attributes :id, :title, :hourly_rate, :job_description, :number_of_children, :specific_days_needed, :required_to_drive, :parent_id
 
 end
