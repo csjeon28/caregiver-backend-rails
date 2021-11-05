787ea776433aaebe40007c492e4d5822eecab6c3
Christine Jeon <csjeon28@gmail.com>
2021-11-05 16:20:52 -0400

    added method for all jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,67 +1,54 @@
 class JobsController < ApplicationController
-    before_action :authorized, only: [:create, :update, :destroy]
-    before_action :set_job, only: [:show, :update, :destroy]
-    # before_action :set_caregiver, only: [:create]
-  
-    # GET /jobs
-    def index
-      if params[:caregiver_id].present?
-        @caregiver = Caregiver.find(params[:caregiver_id])
-        @jobs = @caregiver.jobs.caregiver_jobs(@caregiver.id)
-      end
-  
-      if params[:parent_id].present?
-        @parent = Parent.find(params[:parent_id])
-        @jobs = @parent.jobs.parent_jobs(@parent.id)
-      end
-  
-      render json: @jobs
-    end
-  
-    # GET /jobs/1
-    def show
-      render json: @job
-    end
-  
-    # POST /jobs
-    def create
-      @job = @parent.jobs.new(job_params)
-    #   //not sure if i need this code below
-      @job.parent_id = @parent.id  
-      @job.save
-        render json: @job, status: :created
-    #   else
-    #     render json: @job.errors, status: :unprocessable_entity
-    #   end
-    end
-  
-    # PATCH/PUT /jobs/1
-    def update
-      if @job.update(job_params)
-        render json: @job
-      else
-        render json: @job.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /jobs/1
-    def destroy
-      @job.destroy
-    end
-  
-    private
-  
-    def set_job
-      @job = Job.find(params[:id])
-    end
-  
-    # def set_caregiver
-    #   @caregiver = Caregiver.find(params[:caregiver_id])
-    # end
-  
-    def job_params
-        params.require(:job).permit(:title, :city, :state, :job_description, :number_of_children, 
-        :hourly_rate, :required_to_drive, :specific_days_needed, :caregiver_id, :parent_id)
-    end
+  before_action :authorized, only: [:auto_login, :destroy]
+  skip_before_action :authorized, only: [:create, :all_jobs]
+  before_action :set_job, only: [:destroy]
+  before_action :set_parent, only: [:create, :index]
 
+  # GET /all-jobs
+  def all_jobs
+    @jobs = Job.all
+    render json: @jobs
+  end
+
+  # GET /:id/jobs
+  def index
+    if params[:parent_id].present?
+      set_parent
+      @jobs = @parent.jobs
+    end
+  
+    render json: @jobs
+  end
+  
+  # POST /jobs
+  def create
+    @job = @parent.jobs.new(job_params)
+    @job.parent_id = @parent.id
+    if @job.valid?
+    @job.save
+      render json: @job, status: :created
+    else
+      render json: {error: @job.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+  
+  # DELETE /jobs/1
+  def destroy
+    @job.destroy
+  end
+  
+  private
+  
+  def set_parent
+    @parent = Parent.find(params[:parent_id])
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+  
+  def job_params
+      params.require(:job).permit(:title, :job_description, :number_of_children, 
+      :hourly_rate, :required_to_drive, :specific_days_needed, :parent_id)
+  end
 end
