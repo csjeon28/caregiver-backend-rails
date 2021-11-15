59c45b054894f8a13976512c8e46ab40157e9731
Christine Jeon <csjeon28@gmail.com>
2021-11-15 16:49:15 -0500

    added validations for caregiver id

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,4 +1,6 @@
 class Candidate < ApplicationRecord
     belongs_to :job
-    belongs_to :caregiver
+    belongs_to :caregiver, optional: true
+
+    validates_uniqueness_of :caregiver_id, scope: [:job_id]
 end
