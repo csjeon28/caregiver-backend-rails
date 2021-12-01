5bf66d979684dbe5ed9f050ae9332d09de94425b
Christine Jeon <csjeon28@gmail.com>
2021-12-01 14:12:20 -0500

    added fallback controller for deploying

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,7 @@ Rails.application.routes.draw do
   post "parent/login", to:"parents#login"
   get "parent/auto_login", to:"parents#auto_login"
   patch "parents/:id/jobs", to:"jobs#update"
+
+  root "fallback#index"
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
